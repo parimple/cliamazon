05afc6b9244d97e21cbb266c2b86208deaec73f7
parimple <patrykpyzel@gmail.com>
2019-11-18 12:01:45 +0100

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-README
+- List all files in an S3 Bucket
+
+`node index.js ls
+`
+
+- Upload a local file to a defined location in the bucket
+
+`node index.js push ./data/file.txt
+`
+- List an AWS buckets files that match a "filter" regex
+
+`node index.js lsregex folder/1574065792603_file.txt
+`
+
+- Delete all files matching a regex from a bucket
+
+`node index.js rmregex folder/1574065954705_file.txt
+`
--- index.js
+++ index.js
@@ -3,149 +3,124 @@ const fs = require('fs');
 const path = require('path');
 const Regex = require("regex");
 
-//configuring the AWS environment
-
-
 bucketaws = 'lcloud-427-ag';
-
-AWS_ACCESS_KEY_ID='AKIAS464CIUFY3NKMV5H';
-AWS_SECRET_ACCESS_KEY='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';
+AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 
 AWS.config.update({
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY
 });
 
-var s3 = new AWS.S3();
-var filePath = "./data/file.txt";
-
-//configuring parameters
-var params = {
-    Bucket: bucketaws,
-    Body : fs.createReadStream(filePath),
-    Key : "folder/"+Date.now()+"_"+path.basename(filePath)
-};
-
-var myArgs = process.argv.slice(2);
+const s3 = new AWS.S3();
+const myArgs = process.argv.slice(2);
 
 console.log('args: ', myArgs);
 
-    switch (myArgs[0]) {
-        case "push":
-            if (myArgs[1]) {
-                var params = {
-                    Bucket: bucketaws,
-                    Body : fs.createReadStream(myArgs[1]),
-                    Key : "folder/"+Date.now()+"_"+path.basename(myArgs[1])
-                };
-                s3.upload(params, function (err, data) {
-                    //handle error
-                    if (err) {
-                        console.log("Error", err);
+switch (myArgs[0]) {
+    case "push":
+        if (myArgs[1]) {
+            var params = {
+                Bucket: bucketaws,
+                Body : fs.createReadStream(myArgs[1]),
+                Key : "folder/"+Date.now()+"_"+path.basename(myArgs[1])
+            };
+            s3.upload(params, function (err, data) {
+                if (err) {
+                    console.log("Error", err);
+                }
+                if (data) {
+                    console.log("Uploaded in:", data.Location);
+                }
+            });
+        }
+        break;
+
+    case "ls":
+        async function listAllObjectsFromS3Bucket(bucket) {
+            let isTruncated = true;
+            let marker;
+            while(isTruncated) {
+                let params = { Bucket: bucket };
+                if (marker) params.Marker = marker;
+                try {
+                    const response = await s3.listObjects(params).promise();
+                    response.Contents.forEach(item => {
+                        console.log(item.Key);
+                    });
+                    isTruncated = response.IsTruncated;
+                    if (isTruncated) {
+                        marker = response.Contents.slice(-1)[0].Key;
                     }
-                    //success
-                    if (data) {
-                        console.log("Uploaded in:", data.Location);
-                    }
-                });
+                } catch(error) {
+                    throw error;
+                }
             }
+        }
+        listAllObjectsFromS3Bucket(bucketaws);
+        break;
 
-            break;
-
-        case "ls":
-            async function listAllObjectsFromS3Bucket(bucket) {
-                let isTruncated = true;
-                let marker;
-                while(isTruncated) {
-                    let params = { Bucket: bucket };
-                    if (marker) params.Marker = marker;
-                    try {
-                        const response = await s3.listObjects(params).promise();
-                        response.Contents.forEach(item => {
+    case "lsregex":
+        async function listRegexObjectsFromS3Bucket(bucket, regext) {
+            const regex = new Regex(regext);
+            let isTruncated = true;
+            let marker;
+            while(isTruncated) {
+                let params = { Bucket: bucket };
+                if (marker) params.Marker = marker;
+                try {
+                    const response = await s3.listObjects(params).promise();
+                    response.Contents.forEach(item => {
+                        if (regex.test(item.Key)) {
                             console.log(item.Key);
-                        });
-                        isTruncated = response.IsTruncated;
-                        if (isTruncated) {
-                            marker = response.Contents.slice(-1)[0].Key;
                         }
-                    } catch(error) {
-                        throw error;
+                    });
+                    isTruncated = response.IsTruncated;
+                    if (isTruncated) {
+                        marker = response.Contents.slice(-1)[0].Key;
                     }
+                } catch(error) {
+                    throw error;
                 }
             }
+        }
+        listRegexObjectsFromS3Bucket(bucketaws, myArgs[1]);
+        break;
 
-            listAllObjectsFromS3Bucket(bucketaws);
-
-            break;
-
-        case "lsregex":
-            async function listRegexObjectsFromS3Bucket(bucket, regext) {
-                const regex = new Regex(regext);
-                let isTruncated = true;
-                let marker;
-                while(isTruncated) {
-                    let params = { Bucket: bucket };
-                    if (marker) params.Marker = marker;
-                    try {
-                        const response = await s3.listObjects(params).promise();
-                        response.Contents.forEach(item => {
-
-                            if (regex.test(item.Key)) {
-                                console.log(item.Key);
-                            }
-
-                        });
-                        isTruncated = response.IsTruncated;
-                        if (isTruncated) {
-                            marker = response.Contents.slice(-1)[0].Key;
+    case "rmregex":
+        async function rmregexObjectsFromS3Bucket(bucket, regext) {
+            const regex = new Regex(regext);
+            let isTruncated = true;
+            let marker;
+            while(isTruncated) {
+                let params = { Bucket: bucket };
+                if (marker) params.Marker = marker;
+                try {
+                    const response = await s3.listObjects(params).promise();
+                    response.Contents.forEach(item => {
+                        if (regex.test(item.Key)) {
+                            console.log('deleted file: ', item.Key);
+                            var params = {  Bucket: bucketaws, Key: item.Key };
+                            s3.deleteObject(params, function(err, data) {
+                                if (err) console.log(err, err.stack);  // error
+                                else     console.log();                 // deleted
+                            });
                         }
-                    } catch(error) {
-                        throw error;
+                    });
+                    isTruncated = response.IsTruncated;
+                    if (isTruncated) {
+                        marker = response.Contents.slice(-1)[0].Key;
                     }
+                } catch(error) {
+                    throw error;
                 }
             }
-            listRegexObjectsFromS3Bucket(bucketaws, myArgs[1]);
+        }
+        rmregexObjectsFromS3Bucket(bucketaws, myArgs[1]);
+        break;
 
-            break;
-
-        case "rmregex":
-
-            async function listRmregexObjectsFromS3Bucket(bucket, regext) {
-                const regex = new Regex(regext);
-                let isTruncated = true;
-                let marker;
-                while(isTruncated) {
-                    let params = { Bucket: bucket };
-                    if (marker) params.Marker = marker;
-                    try {
-                        const response = await s3.listObjects(params).promise();
-                        response.Contents.forEach(item => {
-
-                            if (regex.test(item.Key)) {
-                                console.log('deleted file: ', item.Key);
-                                var params = {  Bucket: bucketaws, Key: item.Key };
-                                s3.deleteObject(params, function(err, data) {
-                                    if (err) console.log(err, err.stack);  // error
-                                    else     console.log();                 // deleted
-                                });
-
-                            }
-
-                        });
-                        isTruncated = response.IsTruncated;
-                        if (isTruncated) {
-                            marker = response.Contents.slice(-1)[0].Key;
-                        }
-                    } catch(error) {
-                        throw error;
-                    }
-                }
-            }
-            listRmregexObjectsFromS3Bucket(bucketaws, myArgs[1]);
-            break;
-
-        default:
-            process.stdout.write('Typed command is not accurate');
-    }
+    default:
+        process.stdout.write('Typed command is not accurate');
+}
 
 
